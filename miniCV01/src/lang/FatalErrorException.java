1f08c83fd33c6d48cd0455e41531e540f1083955
zip <remtus0824mami@gmail.com>
2019-08-20 15:43:05 +0900

    miniC ver.01
    減算式の計算コンパイラ、コメントアウトの字句解析追加

---
+++ miniCV01/src/lang/FatalErrorException.java
@@ -0,0 +1,5 @@
+package lang;
+
+// プログラム中で致命的なエラーが生じたときに投げる例外
+@SuppressWarnings("serial")
+public class FatalErrorException extends Exception {}
---
+++ miniCV01/src/lang/c/CToken.java
@@ -0,0 +1,13 @@
+package lang.c;
+
+import lang.SimpleToken;
+
+// 字句として認識しなければならないカテゴリを定義するクラス(何行目、何文字目、綴りの情報を記録)
+public class CToken extends SimpleToken {
+	public static final int TK_PLUS			= 2;				// +
+	public static final int TK_MINUS 		= 3;				// -
+
+	public CToken(int type, int lineNo, int colNo, String s) {
+		super(type, lineNo, colNo, s);
+	}
+}
